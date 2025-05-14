b98bd772f94f149f012900cc54c6902c4cc28cfe
Dr-nikson <game.nik.nik@gmail.com>
2025-05-14 23:46:29 +0200

    Add some typetests

--- packages/effect-crypto/src/utils/effectUtils.test-d.ts
+++ packages/effect-crypto/src/utils/effectUtils.test-d.ts
@@ -21,3 +21,36 @@ test("EffectUtils.mapParN should work ", () => {
     EffectUtils.mapParN([a, b, c], ([a, b, c]) => ({ a, b, c }) as Entity),
   );
 });
+
+test("EffectUtils.mapParN should not compile if the input's error type is not a NonEmptyArray", () => {
+  const a: Either.Either<number, string[]> = Either.right(1);
+  const b: Either.Either<string, string[]> = Either.left(["2"]);
+  const c: Either.Either<boolean, string[]> = Either.right(true);
+
+  assertType<Either.Either<Entity, ErrorType>>(
+    // @ts-expect-error string[] is not assignable NonEmptyArray<string>
+    EffectUtils.mapParN([a, b, c], ([a, b, c]) => ({ a, b, c }) as Entity),
+  );
+});
+
+test("EffectUtils.mapParN should compile if the input's error type is correct NonEmptyArray", () => {
+  const a = Either.right(1);
+  const b = Either.left(Array.make("2"));
+  const c = Either.right(true);
+
+  assertType<Either.Either<Entity, ErrorType>>(
+    EffectUtils.mapParN([a, b, c], ([a, b, c]) => ({ a, b, c }) as Entity),
+  );
+});
+
+
+test("EffectUtils.mapParN should not compile if the input's error types is not aligned", () => {
+  const a = Either.left(Array.make(false));
+  const b = Either.left(Array.make("2"));
+  const c = Either.right(true);
+
+  assertType<Either.Either<Entity, ErrorType>>(
+    // @ts-expect-error error types should be aligned
+    EffectUtils.mapParN([a, b, c], ([a, b, c]) => ({ a, b, c }) as Entity),
+  );
+});
